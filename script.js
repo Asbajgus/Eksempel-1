43507cdd5c508f460a159f7b219e400a4e5d208c
Peter André Altier <petera113.pa@gmail.com>
2025-02-25 13:29:31 +0100

    oki

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ function updateImage(part, direction) {
     imgElement.src = partImages[currentIndex[part]];
 }
 
+function noLayout() {
+    document.getElementById('cards').classList.value = '';
+}
+
+function horizontalLayout() {
+    noLayout();
+    document.getElementById('cards').classList.add('horizontal');
+}
+
+function verticalLayout() {
+    noLayout();
+    document.getElementById('cards').classList.add('vertical');
+}
+
+function gridLayout() {
+    noLayout();
+    document.getElementById('cards').classList.add('grid');
+}
